7d8ebd796c020a73b7c5e91aafc908192bde2b0d
NikiTsPetkov <43531588+NikiTsPetkov@users.noreply.github.com>
2023-07-18 18:11:59 +0300

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,14 @@
 const Cards = ({day}) =>{
     return(
-        <div className="weather-card">
-            <h3>{day.day.mintemp_c} °C / {day.day.maxtemp_c} °C</h3>
-            <img src={day.day.condition.icon} alt={day.day.condition.text} />
-            <h4>{day.day.condition.text}</h4>
-            <p>{day.date}</p>
+        <div className="weather-card weather-three-days">
+            <div className="small-screen">
+                <h3>{day.day.mintemp_c} °C / {day.day.maxtemp_c} °C</h3>
+                <img src={day.day.condition.icon} alt={day.day.condition.text} />
+            </div>
+            <div className="small-screen">
+                <h4>{day.day.condition.text}</h4>
+                <p>{day.date}</p>
+            </div>
         </div>
     )
 }
